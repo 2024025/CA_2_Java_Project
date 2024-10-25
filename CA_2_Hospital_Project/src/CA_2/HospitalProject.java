01d70c6292eb780257322953a13c71c3480c6d5d
2024025 <2024025@student.cct.ie>
2024-10-25 13:01:30 +0100

    Starting Construction of the Hospital Project Class
    Creating the positions and departments for employees

--- CA_2_Hospital_Project/src/CA_2/HospitalProject.java
+++ CA_2_Hospital_Project/src/CA_2/HospitalProject.java
@@ -1,12 +1,42 @@
 package CA_2;
 
+
 import java.util.Scanner;
 
 public class HospitalProject {
+  
+    private Scanner myKb = new Scanner(System.in);         
+    
+    private Manager[] manager = {
+        new Manager("Director"),
+        new Manager("Operations Manager"),
+        new Manager("Supervisor"),
+        new Manager("Department Head"),
+        new Manager("Research Coordinator"),
+        new Manager("Ward Supervisor"),
+        new Manager("Doctor"),
+        new Manager("Surgeon"),
+        new Manager("General Practitioner"),
+        new Manager("Nurse"),
+        new Manager("Nursing Assistant"),
+        new Manager("Medical Technician"),
+    };
+    
+    private Department[] department = {
+        new Department("Executive"),
+        new Department("Finances"),
+        new Department("Critical Care Unit - CTI"),
+        new Department("Intensive Care Unit - ICU"),
+        new Department("Emergency"),
+        new Department("Primary Care"),
+        new Department("Oncology"),
+        new Department("Laboratory"),
+        new Department("Cardiology"),
+        new Department("Radiology"),
+        new Department("Anaesthetist"),
+        new Department("Surgery"),
+        new Department("Orthopaedic"),
+    };
 
-    public static void main(String[] args) {
-       
-        Scanner myKb = new Scanner(System.in);
-        
-    }
+   
 }
