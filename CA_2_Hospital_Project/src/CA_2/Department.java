f9d169b0fb6dd6050155f3f91c86e1ee844091ff
2024025 <2024025@student.cct.ie>
2024-10-25 11:15:44 +0100

    Department and Emplyee Classes  setted up

--- CA_2_Hospital_Project/src/CA_2/Department.java
+++ CA_2_Hospital_Project/src/CA_2/Department.java
@@ -1,6 +1,20 @@
 package CA_2;
 
-
 public class Department {
+    private String department;
 
+    public Department(String department) {
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    
+    
+    
 }
--- CA_2_Hospital_Project/src/CA_2/Employee.java
+++ CA_2_Hospital_Project/src/CA_2/Employee.java
@@ -1,6 +1,37 @@
 package CA_2;
 
-
 public class Employee {
+    private String name;
+    private String position;
+    private Department department;
 
+    public Employee(String name, String position, Department department) {
+        this.name = name;
+        this.position = position;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+    
+    
+    
+    
 }
