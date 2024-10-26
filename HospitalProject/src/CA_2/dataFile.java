968e5b56d7afde805007ff19ca71745eaa11c8e5
2024025 <2024025@student.cct.ie>
2024-10-27 00:51:24 +0100

    Update dataFile.java
    Adding a new method to write the employee report in a new file.

--- HospitalProject/src/CA_2/dataFile.java
+++ HospitalProject/src/CA_2/dataFile.java
@@ -1,8 +1,11 @@
 package CA_2;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class dataFile {
 
@@ -17,6 +20,8 @@ public class dataFile {
         return surnames;
     }
 
+    
+//    Method responsible to read the file.
     private void readFile(String fileName) {
         try {
             BufferedReader br = new BufferedReader(new FileReader(fileName));
@@ -39,6 +44,7 @@ public class dataFile {
         }
     }
 
+//Constructor resposible to count the lines and initiate the arrays   
     public dataFile(String fileName) {
         int countLines = linesInFile(fileName);
         if (countLines == 0) {
@@ -50,6 +56,8 @@ public class dataFile {
         readFile(fileName);
     }
 
+    
+//Method to count line by line in the file    
     private int linesInFile(String fileName) {
         int lineCount = 0;
         try {
@@ -63,5 +71,20 @@ public class dataFile {
         }
         return lineCount;
     }
-
+    
+    
+//Method responsible to write in a new file the names, surnames in a new file.    
+    public void writeEmployeeReport(String fileName, List<Employee> employees){
+        try{
+            BufferedWriter wr = new BufferedWriter(new FileWriter(fileName, true));
+            for(Employee employee: employees){
+                wr.write(employee.toString());
+                wr.newLine();
+            }
+            wr.close();
+            System.out.println("Employee Report Successfully Created in: " +fileName);
+        }catch(IOException e){
+            System.out.println("Error when writing the file: " + e.getMessage());
+        }
+    }
 }
