ae3b1ec37dc7b4ba9a3268f976c7e6f93d7567f3
2024025 <2024025@student.cct.ie>
2024-11-05 00:48:17 +0000

    Create Patient.java
    Patient class created with the purpose of adding all the details of the care provided to the patient and having the entire record of the patient's admission to the hospital, including time, name, health condition, diagnosis, etc.
    
    This information will then be inserted into the list, so that it can have the patient record and finally have the possibility of generating a new text file with the reports of all the patients treated, allowing future research to be carried out.

---
+++ HospitalProject/src/CA_2/Patient.java
@@ -0,0 +1,85 @@
+package CA_2;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Patient extends Person {
+
+    private int age;
+    private LocalDateTime admissionDate;
+    private String diagnosis;
+    private Department department;
+    private String attendingDoctor;
+    private int staydays;
+    private double dailyRate;
+    private LocalDateTime dischargeDate;
+
+    public Patient(int age, LocalDateTime admissionDate, String diagnosis,
+            Department department, String attendingDoctor, int staydays,
+            double dailyRate, LocalDateTime dischargeDate, String firstName, String lastname) {
+
+        super(firstName, lastname);
+        this.age = age;
+        this.admissionDate = admissionDate;
+        this.diagnosis = diagnosis;
+        this.department = department;
+        this.attendingDoctor = attendingDoctor;
+        this.staydays = staydays;
+        this.dailyRate = dailyRate;
+        this.dischargeDate = dischargeDate;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public LocalDateTime getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public String getAttendingDoctor() {
+        return attendingDoctor;
+    }
+
+    public int getStaydays() {
+        return staydays;
+    }
+
+    public double getDailyRate() {
+        return dailyRate;
+    }
+
+    public LocalDateTime getDischargeDate() {
+        return dischargeDate;
+    }
+
+    public double treatmentCostCalculator() {
+        return staydays * dailyRate;
+    }
+
+    public LocalDateTime DischargeDateCalculator() {
+        return admissionDate.plusDays(staydays);
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        return "\nPatient: " + getFirstName() + " " + getLastname()
+                + "\nAge: " + age
+                + "\nCondition: " + diagnosis
+                + "\nDepartment: " + department
+                + "\nAttending Doctor: " + attendingDoctor
+                + "\nStay Days: " + staydays
+                + "\nDischarge Date: " + dischargeDate.format(formatter)
+                + "\nDaily Rate: € " + dailyRate
+                + "\nTotal Cost: € " + treatmentCostCalculator();
+    }
+}
