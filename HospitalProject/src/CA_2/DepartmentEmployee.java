cdb91d545dad02cb17ee4414bca24addcdd92c0d
2024025 <2024025@student.cct.ie>
2024-11-06 12:57:21 +0000

    Commenting classes: DepartmentEmployee and Employee

--- HospitalProject/src/CA_2/DepartmentEmployee.java
+++ HospitalProject/src/CA_2/DepartmentEmployee.java
@@ -1,10 +1,19 @@
 package CA_2;
 
+/*This class is responsible for representing the administrative departments in the hospital,
+where the employees will be located. This class extends the DepartmentPatient class, thus
+making it a broad and complete class, with all departments into administrative and clinical.
+I will be able then to have a better organization of the system, when I link an employee to a
+department, without running the risk of randomly creating an employee, who will take over a clinical
+department, for example: A doctor would not be likely to work in Human Resource Dpt.
+This class will be called in the HospitalApplication class.
+ */
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DepartmentEmployee extends DepartmentPatient {
+//Constant list of departments for administratives employees
 
     private static final List<Department> employeeDepartments = Arrays.asList(
             new Department("Executive"),
@@ -20,13 +29,14 @@ public class DepartmentEmployee extends DepartmentPatient {
             new Department("Billing and Insurance")
     );
 
-    public DepartmentEmployee(String name) {
+    public DepartmentEmployee(String name) {//Constructor created to call the superclass constructor DepartmentPatient
         super(name);
     }
 
+//    Method creted to combine the list of departments of patients and employees
     public static List<Department> getEmployeeDepartments() {
-        List<Department> allDepartments = new ArrayList<>(DepartmentPatient.getPatientDepartments());
-        allDepartments.addAll(employeeDepartments);
-        return allDepartments;
+        List<Department> allDepartments = new ArrayList<>(DepartmentPatient.getPatientDepartments());//creating a new list that handle the patient Departments
+        allDepartments.addAll(employeeDepartments);//Adding the list of employee departments 
+        return allDepartments; //returning the full list, combined between both departments.
     }
 }
--- HospitalProject/src/CA_2/Employee.java
+++ HospitalProject/src/CA_2/Employee.java
@@ -1,54 +1,73 @@
 package CA_2;
 
-public class Employee extends Person {
+/*This class is responsible for storing all information related to employees, such as: department,
+hours worked, monthly salary payment, etc.
+This class is an extension of Person class, where it will absorb data from first and last names.
+This class will be called in the GenerateRandomEmployee and HospitalApplication classes.
+*/
 
+public class Employee extends Person {
+//Private attributes to store informations about the Employees.
     private Manager manager;
     private Department department;
     private int hoursWorked;
     private double monthlyPayment;
 
+//    Constructor used to initialize all the attributes
     public Employee(String firstName, String lastName, Manager manager, Department department) {
         super(firstName, lastName);
         this.manager = manager;
         this.department = department;
 
-        if (manager == null) {
-            throw new IllegalArgumentException("Manager can not be null!");
+        if (manager == null) {//Conditions created to checking if the manager is null
+            throw new IllegalArgumentException("Manager can not be null!");//If this condition is true, a exception will be throw and a message will show up to warning the user
         }
-        if (department == null) {
-            throw new IllegalArgumentException("Department can not be null!");
+        if (department == null) {//Conditions created to checking if the department is null
+            throw new IllegalArgumentException("Department can not be null!");//If this condition is true, a exception will be throw and a message will show up to warning the user
         }
-        setRandomHoursWorked();
-        calculateSalary();
+        
+        
+        //If the condition is false, the methods below will be called.
+        
+        setRandomHoursWorked();//calling the method, to generate a random amount of hours
+        calculateSalary();//calling the method to calculate the salary.
     }
 
-    private void setRandomHoursWorked() {
-        Salary salaryCalculator = new Salary();
-        this.hoursWorked = salaryCalculator.generateRandomWorkedHours(80, 160);
+    
+//    Getters and Setters, to allow other class import this attributes
+    
+    private void setRandomHoursWorked() {//Method created to define a random amount of hours worked by employees
+        Salary salaryCalculator = new Salary();//Creating a salary object to calculate the salary.
+        this.hoursWorked = salaryCalculator.generateRandomWorkedHours(80, 160);//Defining the amount of hours allowed to work, min 80 and max 160
     }
 
-    public Manager getManager() {
+    
+    public Manager getManager() {//Getter used to be accessible in other classes.
         return manager;
     }
 
-    public double getMonthlyPayment() {
+    public double getMonthlyPayment() {//Getter used to be accessible in other classes.
         return monthlyPayment;
     }
 
-    public Department getDepartment() {
+    public Department getDepartment() {//Getter used to be accessible in other classes.
         return department;
     }
 
+//    Method create to calculate the salary for each employee, accordingly to the managerType Position .
     private void calculateSalary() {
-        Salary salaryCalculator = new Salary();
-        this.monthlyPayment = salaryCalculator.calculateMonthlyPayment(manager.getPosition().getTitle(), hoursWorked);
+        Salary salaryCalculator = new Salary();//Creating a salary object to calculate the salary.
+        this.monthlyPayment = salaryCalculator.calculateMonthlyPayment(manager.getPosition().getTitle(), hoursWorked);//Calculating the monthly payment accordingly to the manager type position and hours worked
     }
 
+//    Method create in case there is any needs to enter the hours manually, not used in the HospitalApplication, yet.
     public void setHoursWorked(int hours) {
         this.hoursWorked = hours;
-        calculateSalary();
+        calculateSalary();//Recalculating the salary, accordingly with the hours entered.
     }
 
+    
+//    toString() method, used to print the strings about the employee, its hours and salary.
     @Override
     public String toString() {
         return "Employee: " + getFirstName() + " " + getLastname() + " - "
